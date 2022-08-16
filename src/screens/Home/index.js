93111e3639def2993ea57247b596ac485b9b987a
dsrocha <dsrocha@stefanini.com>
2022-08-16 00:47:46 -0300

    Inicio do projeto

---
+++ src/screens/Home/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container, ViewArea, InputButton, InputText, SimpleText } from "./styles";
+export default () => {
+
+    return (
+        <Container>
+            <ViewArea>
+                <SimpleText>Danilo da Silva Rocha</SimpleText>
+            </ViewArea>
+        </Container>
+    );
+};
